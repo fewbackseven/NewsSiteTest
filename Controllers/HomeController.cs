3e7ba9ee8c78ad3ec02230d09a0683ca9e56993f
Harshith C <fewbackseven@gmail.com>
2021-12-26 23:03:02 +0530

    Getting data from forms

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using NewsSiteTest.Models;
+
 
 namespace NewsSiteTest.Controllers
 {
     public class HomeController : Controller
     {
+       
         public ActionResult Index()
         {
             return View();
@@ -20,11 +24,38 @@ namespace NewsSiteTest.Controllers
             return View();
         }
 
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult About(CheckSignIn objUserData)
+        {
+            DataTable dtUserData = new DataTable();
+            CheckSignIn objCreateUser = new CheckSignIn();
+            bool sResult;
+            sResult = objCreateUser.CreateUser(objUserData);
+
+            if (sResult)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ResultMsg = "Could not create User. Please enter the correct Details";
+                return View();
+            }
+
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
     }
 }
---
+++ Models/CheckSignIn.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.OleDb;
+
+namespace NewsSiteTest.Models
+{
+    
+    public class CheckSignIn
+    {
+        public string sFullName { get; set; }
+        public string sEmailId { get; set; }
+        public string sMobileNumber { get; set; }
+        public string sPassWord { get; set; }
+
+
+
+        string sConString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+
+        public bool CreateUser(CheckSignIn objUser)
+        {
+            try
+            {
+                OleDbParameter[] objParam = new OleDbParameter[5];
+                int iParCount = 0;
+
+                objParam[iParCount] = new OleDbParameter("@UserFullName", OleDbType.VarChar);
+                objParam[iParCount].Value = objUser.sFullName;
+                objParam[iParCount].Direction = ParameterDirection.Input;
+                iParCount += 1;
+
+                objParam[iParCount] = new OleDbParameter("@EmailId", OleDbType.VarChar);
+                objParam[iParCount].Value = objUser.sEmailId;
+                objParam[iParCount].Direction = ParameterDirection.Input;
+                iParCount += 1;
+
+                objParam[iParCount] = new OleDbParameter("@MobileNumber", OleDbType.VarChar);
+                objParam[iParCount].Value = objUser.sMobileNumber;
+                objParam[iParCount].Direction = ParameterDirection.Input;
+                iParCount += 1;
+
+                objParam[iParCount] = new OleDbParameter("@PassWord", OleDbType.VarChar);
+                objParam[iParCount].Value = objUser.sPassWord;
+                objParam[iParCount].Direction = ParameterDirection.Input;
+                iParCount += 1;
+
+                if (objUser.sFullName != "")
+                    return true;
+                else
+                    return false;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
